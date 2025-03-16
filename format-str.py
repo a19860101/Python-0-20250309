75dcb1f7a2877b17f59c1f8fb6c5a3e30bb219e2
Zac <a19860101@gmail.com>
2025-03-16 12:16:39 +0800

    20250316

---
+++ format-str.py
@@ -0,0 +1,28 @@
+# %
+#
+# a =  'hello %10s'
+# b = 'hello %d'
+# c = 'hello %+10.3f'
+# d = 'hello %b'
+#
+#
+# msg = 'hello %s ! 今天氣溫為 %.1f度C'
+# print(msg % ('Max',13))
+
+# format()
+# msg = 'hello {:@^12.3f}'
+# print(msg.format(3.12345678))
+
+# msg = 'hello {} ! 今天氣溫為 {}度C'
+# msg = 'hello {1} ! 今天氣溫為 {0}度C'
+# msg = 'hello {name} ! 今天氣溫為 {deg:.1f}度C'
+
+# print(msg.format(name='John',deg=10))
+
+# f-string
+
+# name = 'John'
+# deg = 12
+# print(f'Hello {name:-^12s} 今天氣溫為{deg:.1f}度C')
+
+
---
+++ str.py
@@ -0,0 +1,60 @@
+
+# a = 'hello "John"'
+# a = "hello 'john'"
+
+# 跳脫字元
+
+# a = 'hello \'john\''
+# a = "hello \"John\""
+# a = 'hello\njohn'
+# a = 'hello\tjohn\nhello\tmary'
+# a = 'name:\tJohn\nemail:\tasdf@gmail.com'
+
+# a = 'C:\\Users\\B-21\\Desktop\\Python'
+# a = r'C:\Users\B-21\Desktop\Python'
+# print(a)
+
+# String index
+# s = 'helloworld!!'
+# print(s[5])
+# print(s[2:6])
+# print(s[3:])
+# print(s[:7])
+# print(s[-1])
+# print(s[-2])
+# print(s[-3])
+# print(s[-4:-1])
+# print(s[0:6:2])
+
+# String Method
+s = 'Hello'
+
+# len()
+# print(len(s))
+
+# count()
+# print(s.count('p'))
+
+# index()
+# print(s.index('l'))
+
+# isalpha()
+# print(s.isalpha())
+# isdigit()
+# print(s.isdigit())
+# endswith()
+print(s.endswith('o'))
+# startswith()
+print(s.startswith('H'))
+
+
+# upper()
+# lower()
+# capitalize()
+
+
+# replace()
+# find()
+
+# split()
+# strip()
