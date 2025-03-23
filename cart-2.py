5690a0b77475f6f4d7076f4a4f06b4162c3a687f
Zac <a19860101@gmail.com>
2025-03-23 16:25:52 +0800

    20250323

--- cart-2.py
+++ cart-2.py
@@ -26,4 +26,17 @@ while True:
     selected = input('請輸入品項編號(按q/Q完成輸入)').lower()
     if selected == 'q':
         break
-    elif
+    elif int(selected) > len(menu):
+        print('請選擇正確的品項編號')
+        continue
+    elif menu[int(selected) - 1] in menu:
+        cart.append(menu[int(selected) - 1])
+
+print('您購買了:\n')
+for item in cart:
+    itemName = item['name'][2:]
+    print(f'{itemName:10}:${item['price']}元')
+    total += item['price']
+
+print('----------------------------------------------')
+print(f'共${total}元')
