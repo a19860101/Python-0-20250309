6777d89feaaba069d15259435906ee5efbd55ee9
Zac <a19860101@gmail.com>
2025-04-20 15:28:24 +0800

    20250420

--- 0420/nike.py
+++ 0420/nike.py
@@ -10,19 +10,24 @@ driver = webdriver.Chrome()
 
 driver.get(url)
 
-n = 0
-while n < 3:
-    driver.execute_script('window.scrollTo(0, document.body.scrollHeight - 1000)')
-    time.sleep(3)
-    n += 1
+# n = 0
+# while n < 3:
+#     driver.execute_script('window.scrollTo(0, document.body.scrollHeight - 1000)')
+#     time.sleep(3)
+#     n += 1
 
-titles = driver.find_elements(By.CLASS_NAME,'product-card__title')
+# product-card__info product-card__title product-card__price
+
+info = driver.find_elements(By.CLASS_NAME,'product-card__info')
 # titles = driver.find_elements(By.CSS_SELECTOR,'.product-card__title')
 
 
 time.sleep(3)
 
-for title in titles:
-    print(title.text)
+# for title in titles:
+#     print(title.text)
+
+for item in info:
+    print(item.text)
 
 
