43e4115d7b77e5900247b9d0808ba27781a2a635
Zac <a19860101@gmail.com>
2025-04-20 15:11:06 +0800

    20250420

--- 0420/nike.py
+++ 0420/nike.py
@@ -4,14 +4,23 @@ import time
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
-url ='https://www.nike.com/tw/w/mens-apparel-6ymx6znik1'
+url ='https://www.nike.com/tw/w/mens-shoes-nik1zy7ok'
 
 driver = webdriver.Chrome()
 
 driver.get(url)
 
+n = 0
+while n < 3:
+    driver.execute_script('window.scrollTo(0, document.body.scrollHeight - 1000)')
+    time.sleep(3)
+    n += 1
+
 titles = driver.find_elements(By.CLASS_NAME,'product-card__title')
 # titles = driver.find_elements(By.CSS_SELECTOR,'.product-card__title')
+
+
+time.sleep(3)
 
 for title in titles:
     print(title.text)
