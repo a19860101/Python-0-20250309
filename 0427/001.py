80337a7db1a1e1c874b63af7c01648fd3011ed2b
Zac <a19860101@gmail.com>
2025-04-27 10:40:38 +0800

    20250427

--- 0427/001.py
+++ 0427/001.py
@@ -17,5 +17,18 @@ soup = BeautifulSoup(htmlfile, 'html.parser')
 imgs = soup.find_all('img')
 
 # imgs = driver.find_elements(By.TAG_NAME, 'img')
-
+# data-landscape-url
 print(imgs)
+i = 1
+for img in imgs:
+    # print(img['data-landscape-url'])
+    path = img['data-landscape-url']
+    name = img['alt']
+    source = requests.get(path)
+    img_source = source.content
+    os.makedirs('images', exist_ok=True)
+
+    with open(f'images/{name}-{i}.jpg', 'wb') as f:
+        f.write(img_source)
+
+    i += 1
