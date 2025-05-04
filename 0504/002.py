b7577abda0a2e3d69beaaf48c3821b3a39c4c8d6
Zac <a19860101@gmail.com>
2025-05-04 11:06:14 +0800

    20250504

--- 0504/002.py
+++ 0504/002.py
@@ -1,6 +1,9 @@
 import pandas as pd
 
+# 一維資料
 data = pd.Series([10,20,22,7,68,55])
+
+# 二維資料
 data2 = pd.DataFrame(
     [
         [1,2,3],
@@ -9,4 +12,33 @@ data2 = pd.DataFrame(
     ]
 )
 
-print(data2)
+data3 = pd.DataFrame(
+    {
+        'name':['Zac','Andy','Tim'],
+        'age':[30,32,28]
+    },
+    index=['A','B','C']
+)
+# print(data3)
+# print(data3.size)
+# print(data3.shape)
+# print(data3.columns)
+# print(data3.index)
+
+# print(data3['name'])
+# print(data3.iloc[0])
+# print(data3.loc['C'])
+
+
+data4 = pd.DataFrame(
+    [
+        {'name':'Zac','age':32},
+        {'name':'Max','age':28},
+        {'name':'Andy','age':30}
+    ]
+)
+
+
+print(data4)
+print(data4.iloc[1])
+print(data4.loc[1])
