d0a0873415e0acec71e63fe774b0e1c272c69391
Zac <a19860101@gmail.com>
2025-05-04 11:26:41 +0800

    20250504

--- 0504/002.py
+++ 0504/002.py
@@ -32,13 +32,29 @@ data3 = pd.DataFrame(
 
 data4 = pd.DataFrame(
     [
-        {'name':'Zac','age':32},
-        {'name':'Max','age':28},
-        {'name':'Andy','age':30}
+        {'name':'Zac','age':32,'gender':'M'},
+        {'name':'Max','age':28,'gender':'M'},
+        {'name':'Andy','age':30,'gender':'M'},
+        {'name':'Amy','age':22,'gender':'F'},
+        {'name':'Laura','age':26,'gender':'F'}
     ]
 )
 
 
-print(data4)
-print(data4.iloc[1])
-print(data4.loc[1])
+# print(data4)
+# print(data4.iloc[1])
+# print(data4.loc[1])
+
+# print(data4['age'].max())
+# print(data4['age'].min())
+# print(data4['age'].mean())
+# print(data4['age'].std())
+# print(data4['age'].describe())
+
+# condition = data4['age'] >= 30
+# print(data4[condition])
+# print(data4[data4['age'] >= 30])
+
+condition = data4['gender'].str.contains('M')
+print(data4[condition])
+
