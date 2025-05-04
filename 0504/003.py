c402e2a34d760c1e028b1ecd14701cd0d97de4cc
Zac <a19860101@gmail.com>
2025-05-04 12:26:44 +0800

    20250504

--- 0504/003.py
+++ 0504/003.py
@@ -1,19 +1,23 @@
 import pandas as pd
 
-# datas = pd.read_csv('Restaurant_C_f.csv')
+datas = pd.read_csv('Restaurant_C_f.csv')
 
-# print(datas.columns)
+print(datas.columns)
 # print(datas['Name'])
 # print(datas['Add'])
 
-# condition = datas['Add'].str.contains('中壢')
-# result = datas[condition]
+condition = datas['Add'].str.contains('中壢')
+# result = result[['Name','Add','Tel']]
+result = datas[condition]
+result = result.drop(['Id','Px','Py'], axis=1)
 #
 # print(result['Name'])
+result.to_excel('chungli.xlsx')
 
-datas = pd.read_csv('COA_OpenData.csv')
-# print(datas.columns)
-print(datas['animal_kind'])
-condition = datas['animal_kind'].str.contains('貓')
-result = datas[condition]
-print(result['animal_Variety'])
+
+# datas = pd.read_csv('COA_OpenData.csv')
+# # print(datas.columns)
+# print(datas['animal_kind'])
+# condition = datas['animal_kind'].str.contains('貓')
+# result = datas[condition]
+# print(result['animal_Variety'])
