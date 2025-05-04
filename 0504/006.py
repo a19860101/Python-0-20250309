cf64190d66c56e1c06a53f1ce97f0ad59c84bef4
Zac <a19860101@gmail.com>
2025-05-04 16:31:45 +0800

    20250504

--- 0504/006.py
+++ 0504/006.py
@@ -3,12 +3,21 @@ import pandas as pd
 plt.rc('font',family='Microsoft Jhenghei')
 datas = pd.read_csv('outbound.csv')
 
+print(datas)
 
-labels = datas.columns[2:-1]
+total = datas.iloc[1]['亞洲地區']
+subtotal = datas.iloc[-1]['小計']
+other = int(total) - int(subtotal)
 
 
-data = datas.iloc[1][2:-1]
-print(data)
+
+labels = list(datas.columns[2:-1])
+labels.append('其他')
+
+data = list(datas.iloc[1][2:-1])
+data.append(other)
+
+print(labels)
 
 plt.pie(
     data,
