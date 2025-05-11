196a5490eb4a4a1aa59681be276efd1834528543
Zac <a19860101@gmail.com>
2025-05-11 14:16:37 +0800

    20250511

--- 0511/qt6-1.py
+++ 0511/qt6-1.py
@@ -10,6 +10,17 @@ widget.resize(400,300)
 
 label = QtWidgets.QLabel(widget)
 label.setText('TEST')
+label.move(30,30)
+
+label2 = QtWidgets.QLabel(widget)
+label2.setText('POIUY')
+label2.move(30,50)
+
+input1 = QtWidgets.QLineEdit(widget)
+input1.move(60, 30)
+
+text1 = QtWidgets.QTextEdit(widget)
+text1.move(100, 100)
 
 
 widget.show()
