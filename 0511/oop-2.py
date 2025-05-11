12db84f5eac11e5b646d1360d00a770138b51389
Zac <a19860101@gmail.com>
2025-05-11 11:37:04 +0800

    20250511

--- 0511/oop-2.py
+++ 0511/oop-2.py
@@ -26,3 +26,22 @@ class Oz:
         result = self.x / 29.573
         result = round(result,2)
         print(f'您的換算結果為{self.x}ml約{result}oz')
+
+    def result(self):
+        if self.s == 'o':
+            self.oz_to_ml()
+        else:
+            self.ml_to_oz()
+
+
+x = input('請輸入轉換的數字:')
+s = input('單位是oz還是ml?(o/m):')
+
+r = Oz(x,s)
+# r.result()
+
+if s == 'o':
+    r.oz_to_ml()
+else:
+    r.ml_to_oz()
+
